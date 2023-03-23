746b8e50b2f090ccad5b0efa5f38b6ebba76f57c
shantongxu <shantongxu@qq.com>
2023-03-24 00:04:12 +0800

    调整 MenuList部分功能到 MenuProvider

--- src/contexts/Menu.tsx
+++ src/contexts/Menu.tsx
@@ -1,4 +1,5 @@
 import { MenuList } from '@/configs/menu';
+import { isSubMenu, isGroupMenu, isExtendMenu } from '@/utils/is';
 import React, { createContext, useMemo, useState } from 'react';
 
 export interface IContextValue {
@@ -6,6 +7,8 @@ export interface IContextValue {
   setMenuCollapsed: ISetFunc;
   menuList: Menu.MenuItemType[];
   setMenuList: ISetFunc<Menu.MenuItemType[]>;
+  mapPathToMenu: Map<string, Menu.ExtendMenuType>;
+  mapKeyToMenu: Map<React.Key, Menu.ExtendMenuType>;
 }
 
 const initValue: IContextValue = undefined as any;
@@ -15,18 +18,59 @@ export const MenuContext = createContext<IContextValue>(initValue);
 
 // Provider
 const MenuProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  let incMenuKey = 0;
   const initMenuList = useMemo(() => MenuList, [MenuList]);
 
-  const [menuList, setMenuList] = useState<IContextValue['menuList']>(initMenuList);
+  const [rawMenuList, setMenuList] = useState<IContextValue['menuList']>(initMenuList);
   const [menuCollapsed, setMenuCollapsed] = useState(false);
+
+  const { mapPathToMenu, mapKeyToMenu, processedMenuList } = useMemo(() => {
+    // 菜单数据处理
+    // 1. key 绑定
+    // 2. 创建 map key--->menuItem
+    // 2. 创建 map path-->menuItem
+    const processeMenu = () => {
+      const mapKeyToMenu = new Map<React.Key, Menu.ExtendMenuType>();
+      const mapPathToMenu = new Map<string, Menu.ExtendMenuType>();
+
+      const addKey = (item: Menu.MenuItemType): Menu.MenuItemType => {
+        if (!item.key) {
+          item.key = String(++incMenuKey);
+        }
+        return { ...item };
+      };
+      const recursive = (list: Menu.MenuItemType[]): Menu.MenuItemType[] => {
+        return list.map((item: any) => {
+          const newItem = addKey(item);
+          if (isSubMenu(newItem) || isGroupMenu(newItem)) {
+            newItem.children = recursive(newItem.children || []);
+          }
+
+          isExtendMenu(newItem) && mapPathToMenu.set(newItem.path, newItem);
+          newItem?.key && mapKeyToMenu.set(newItem.key, newItem as Menu.ExtendMenuType);
+
+          return newItem;
+        });
+      };
+
+      const processedMenuList = recursive(rawMenuList);
+
+      return { processedMenuList, mapKeyToMenu, mapPathToMenu };
+    };
+
+    return processeMenu();
+  }, [rawMenuList]);
+
   const contextValue: IContextValue = useMemo(
     () => ({
-      menuList,
+      menuList: processedMenuList,
       setMenuList,
       menuCollapsed,
-      setMenuCollapsed
+      setMenuCollapsed,
+      mapPathToMenu,
+      mapKeyToMenu
     }),
-    [menuList, menuCollapsed]
+    [rawMenuList, menuCollapsed]
   );
 
   return <MenuContext.Provider value={contextValue} children={children} />;
--- src/layouts/default/components/LeftSider/MenuList.tsx
+++ src/layouts/default/components/LeftSider/MenuList.tsx
@@ -1,5 +1,5 @@
 import { Badge, Menu } from 'antd';
-import React, { useCallback, useContext, useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 
 import type { MenuProps } from 'antd';
 import { isDividerMenu, isExtendMenu, isGroupMenu, isLeafMenu, isSubMenu } from '@/utils/is';
@@ -8,6 +8,7 @@ import Iconfont from '@/components/Iconfont';
 import { createIframeUrl } from '@/utils/iframe';
 import Config from '@/configs';
 import { MenuContext } from '@/contexts/Menu';
+
 type AntdMenuItem = Required<MenuProps>['items'][number];
 
 // 自定义菜单组件, 增加菜单 badge/路由支持
@@ -15,146 +16,18 @@ type AntdMenuItem = Required<MenuProps>['items'][number];
 const MenuList: React.FC = () => {
   const navigate = useNavigate();
 
-  const { menuList: RawMenuList } = useContext(MenuContext);
+  const { menuList: processedMenuList, mapKeyToMenu, mapPathToMenu } = useContext(MenuContext);
 
   let defaultActiveMenu = '/dashboard';
   // 入栈-出栈来匹配
   let defaultOpenKeys: string[] = [];
   // 匹配默认展开菜单是否结束
   let matchOpenKeysEnd = false;
-  let incMenuKey = 0;
 
   const { pathname } = location;
   const [searchParams] = useSearchParams();
 
-  const makeMenuBadge = (menu: Menu.LeafMenuItemType | Menu.SubMenuType) => {
-    if (menu.badge) {
-      if (menu.badge === 'dot') {
-        return (
-          <Badge className="menu-item-badge" offset={[6, 0]} dot>
-            {menu.label}
-          </Badge>
-        );
-      }
-      return (
-        <Badge className="menu-item-badge" offset={[15, 0]} count={menu.badge}>
-          {menu.label}
-        </Badge>
-      );
-    }
-    return menu.label;
-  };
-
-  const makeMenuItems = (
-    menuList: Menu.MenuItemType[],
-    parent?: Menu.ExtendMenuType
-  ): AntdMenuItem[] => {
-    const menuItems: AntdMenuItem[] = [];
-    !matchOpenKeysEnd && parent && defaultOpenKeys.push(parent.key!);
-    menuList.map((rawMenu) => {
-      if (isExtendMenu(rawMenu)) {
-        // 隐藏菜单高亮父级
-        // q: 为什么要用 indexOf?
-        // a: 因为要兼容 /article/update/10 这种路由
-        if (rawMenu.parent && pathname.indexOf(rawMenu.path) === 0) {
-          console.log('dddddd', mapPathToMenu, mapPathToMenu.get(rawMenu.parent), rawMenu.parent);
-
-          defaultActiveMenu = mapPathToMenu.get(rawMenu.parent)?.key || '';
-          matchOpenKeysEnd = true;
-        }
-
-        // 判断当前菜单是否iframe
-        if (rawMenu.type === 'iframe') {
-          const url = searchParams.get('url') || '';
-          if (url === rawMenu.path) {
-            matchOpenKeysEnd = true;
-            defaultActiveMenu = rawMenu.key!;
-          }
-        }
-        if (rawMenu.hideInMenu) {
-          return;
-        }
-      }
-
-      let newMenu: AntdMenuItem | undefined;
-      if (isDividerMenu(rawMenu)) {
-        newMenu = { ...rawMenu };
-      } else if (isGroupMenu(rawMenu)) {
-        // rawMenu是一个联合类型, 赋值时必须明确是联合类型中的哪一个
-        newMenu = { ...(rawMenu as Menu.AntdMenuGroupType) };
-        if (rawMenu.children && rawMenu.children.length > 0) {
-          newMenu.children = makeMenuItems(rawMenu.children);
-        }
-      } else if (isSubMenu(rawMenu) || isLeafMenu(rawMenu)) {
-        let { icon } = rawMenu;
-        if (typeof icon === 'string' && icon.includes('icon-')) {
-          icon = <Iconfont type={icon} />;
-        }
-        newMenu = {
-          // 从rawMenu继承key
-          key: '',
-          ...rawMenu,
-          icon,
-          label: makeMenuBadge(rawMenu),
-          // 编译器在这里就会推断 menu 是属于 MenuItemGroupType 或 SubMenuType
-          children: isSubMenu(rawMenu) ? makeMenuItems(rawMenu.children!, rawMenu) : undefined
-        };
-
-        if (!matchOpenKeysEnd && rawMenu.path === pathname) {
-          defaultActiveMenu = rawMenu.key!;
-          matchOpenKeysEnd = true;
-        }
-      }
-
-      if (newMenu) {
-        newMenu.key = newMenu.key;
-        menuItems.push(newMenu);
-      }
-    });
-
-    !matchOpenKeysEnd && defaultOpenKeys.pop();
-    return menuItems;
-  };
-
-  const { mapPathToMenu, mapKeyToMenu, processedMenuList } = useMemo(() => {
-    // 菜单数据处理
-    // 1. key 绑定
-    // 2. 创建 map key--->menuItem
-    // 2. 创建 map path-->menuItem
-    const processeMenu = () => {
-      const mapKeyToMenu = new Map<React.Key, Menu.ExtendMenuType>();
-      const mapPathToMenu = new Map<string, Menu.ExtendMenuType>();
-
-      const addKey = (item: Menu.MenuItemType): Menu.MenuItemType => {
-        if (!item.key) {
-          item.key = String(++incMenuKey);
-        }
-        return { ...item };
-      };
-      const recursive = (list: Menu.MenuItemType[]): Menu.MenuItemType[] => {
-        return list.map((item: any) => {
-          const newItem = addKey(item);
-          if (isSubMenu(newItem) || isGroupMenu(newItem)) {
-            newItem.children = recursive(newItem.children || []);
-          }
-
-          isExtendMenu(newItem) && mapPathToMenu.set(newItem.path, newItem);
-          newItem?.key && mapKeyToMenu.set(newItem.key, newItem as Menu.ExtendMenuType);
-
-          return newItem;
-        });
-      };
-
-      const processedMenuList = recursive(RawMenuList);
-
-      return { processedMenuList, mapKeyToMenu, mapPathToMenu };
-    };
-
-    return processeMenu();
-  }, [RawMenuList]);
-
-  const menuItems = useMemo(() => makeMenuItems(processedMenuList), [processedMenuList]);
-
+  // 菜单点击事件
   const handleClick: MenuProps['onClick'] = (info) => {
     const menu = mapKeyToMenu.get(info.key);
     console.log('mapKeyToMenu', mapKeyToMenu);
@@ -182,13 +55,105 @@ const MenuList: React.FC = () => {
 
     navigate(menu.path);
   };
-  console.log('defaultOpenKeys', defaultOpenKeys);
-  console.log('defaultActiveMenu', defaultActiveMenu);
+
+  const menuItems = useMemo(() => {
+    const makeMenuBadge = (menu: Menu.LeafMenuItemType | Menu.SubMenuType) => {
+      if (menu.badge) {
+        if (menu.badge === 'dot') {
+          return (
+            <Badge className="menu-item-badge" offset={[6, 0]} dot>
+              {menu.label}
+            </Badge>
+          );
+        }
+        return (
+          <Badge className="menu-item-badge" offset={[15, 0]} count={menu.badge}>
+            {menu.label}
+          </Badge>
+        );
+      }
+      return menu.label;
+    };
+
+    const makeMenuItems = (
+      menuList: Menu.MenuItemType[],
+      parent?: Menu.ExtendMenuType
+    ): AntdMenuItem[] => {
+      const menuItems: AntdMenuItem[] = [];
+      !matchOpenKeysEnd && parent && defaultOpenKeys.push(parent.key!);
+      menuList.map((rawMenu) => {
+        if (isExtendMenu(rawMenu)) {
+          // 隐藏菜单高亮父级
+          // q: 为什么要用 indexOf?
+          // a: 因为要兼容 /article/update/10 这种路由
+          if (rawMenu.parent && pathname.indexOf(rawMenu.path) === 0) {
+            console.log('dddddd', mapPathToMenu, mapPathToMenu.get(rawMenu.parent), rawMenu.parent);
+
+            defaultActiveMenu = mapPathToMenu.get(rawMenu.parent)?.key || '';
+            matchOpenKeysEnd = true;
+          }
+
+          // 判断当前菜单是否iframe
+          if (rawMenu.type === 'iframe') {
+            const url = searchParams.get('url') || '';
+            if (url === rawMenu.path) {
+              matchOpenKeysEnd = true;
+              defaultActiveMenu = rawMenu.key!;
+            }
+          }
+          if (rawMenu.hideInMenu) {
+            return;
+          }
+        }
+
+        let newMenu: AntdMenuItem | undefined;
+        if (isDividerMenu(rawMenu)) {
+          newMenu = { ...rawMenu };
+        } else if (isGroupMenu(rawMenu)) {
+          // rawMenu是一个联合类型, 赋值时必须明确是联合类型中的哪一个
+          newMenu = { ...(rawMenu as Menu.AntdMenuGroupType) };
+          if (rawMenu.children && rawMenu.children.length > 0) {
+            newMenu.children = makeMenuItems(rawMenu.children);
+          }
+        } else if (isSubMenu(rawMenu) || isLeafMenu(rawMenu)) {
+          let { icon } = rawMenu;
+          if (typeof icon === 'string' && icon.includes('icon-')) {
+            icon = <Iconfont type={icon} />;
+          }
+          newMenu = {
+            // 从rawMenu继承key
+            key: '',
+            ...rawMenu,
+            icon,
+            label: makeMenuBadge(rawMenu),
+            // 编译器在这里就会推断 menu 是属于 MenuItemGroupType 或 SubMenuType
+            children: isSubMenu(rawMenu) ? makeMenuItems(rawMenu.children!, rawMenu) : undefined
+          };
+
+          if (!matchOpenKeysEnd && rawMenu.path === pathname) {
+            defaultActiveMenu = rawMenu.key!;
+            matchOpenKeysEnd = true;
+          }
+        }
+
+        if (newMenu) {
+          newMenu.key = newMenu.key;
+          menuItems.push(newMenu);
+        }
+      });
+
+      !matchOpenKeysEnd && defaultOpenKeys.pop();
+      return menuItems;
+    };
+
+    return makeMenuItems(processedMenuList);
+  }, [processedMenuList]);
+
   return (
     <Menu
       onClick={handleClick}
       mode="inline"
-      defaultSelectedKeys={[String(defaultActiveMenu)]}
+      defaultSelectedKeys={[defaultActiveMenu]}
       items={menuItems}
       defaultOpenKeys={defaultOpenKeys}
     />
